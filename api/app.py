fb7a569acfc5755f9d0083ce54c90f6455893c12
Ronaldo Junior <ronaldo@Debian>
2020-05-26 23:13:27 -0300

    filtering data, colors and range colors

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
-import pandas as pd
-import json
+import json, sys
+sys.path.append('/home/ronaldo/Documentos/Python Projects/Covid-19 Analyzer/src/')
 import streamlit as st
 import plotly.express as px
+from analyzer import get_actual_day
+from analyzer import filter_data_distribution_to_show, filter_range_color, filter_map_color
 
 # function to load treated boston dataset
 # st.cache tag indicate that func must be stay in cache to speed up process
@@ -9,63 +11,43 @@ import plotly.express as px
 def get_brazilian_geo_data():
     with open('../data/brazilian geo data/brazil-states.geojson') as file:
         return json.load(file)
-@st.cache
-def get_brazilian_confirmed_cases():
-    return pd.read_csv('../data/brazilian covid-19 data/brazilian_confirmed_series.csv')
-@st.cache
-def get_brazilian_death_cases():
-    return pd.read_csv('../data/brazilian covid-19 data/brazilian_deaths_series.csv')
-@st.cache
-def get_brazilian_recovered_cases():
-    return pd.read_csv('../data/brazilian covid-19 data/brazilian_recovered_series.csv')
-@st.cache
-def get_brazilian_active_cases():
-    return pd.read_csv('../data/brazilian covid-19 data/brazilian_active_series.csv')
 
 brazilian_geo_data = get_brazilian_geo_data()
-brazilian_confirmed_cases = get_brazilian_confirmed_cases()
-brazilian_deaths_cases = get_brazilian_death_cases()
-brazilian_recovered_cases = get_brazilian_recovered_cases()
-brazilian_active_cases = get_brazilian_active_cases()
 
 st.title("Data App - Analisando o impacto da COVID-19 nos estados brasileiros")
 
 # subtitle
 st.markdown("Este Data App foi construido para analisar os diferentes cenários de COVID-19 no Brasil")
 
-options = st.selectbox(
-  'Selecione o que deseja analisar',
-  ('Casos confirmados', 'Número de Mortos', 'Casos Recuperados', 'Casos ativos'))
+option_selected = st.selectbox(
+  'Selecione a opção que deseja analisar',
+  ('CASOS CONFIRMADOS', 'NÚMERO DE MORTOS', 'CASOS RECUPERADOS', 'CASOS ATIVOS'))
 
 st.subheader("COVID-19 no Brasil")
 
-def make_graph_map(dataFrame, options, day):
-    if options == 'Casos Recuperados':
-        color_scale = 'Greens'
-    else:
-        color_scale = 'OrRd'
+def make_graph_map(dataFrame, option, day):
+
+    color_scale = filter_map_color(option)
+    range_color = filter_range_color(option)
+
     fig = px.choropleth(dataFrame, geojson=brazilian_geo_data, locations='Province_State', color=day,
                            color_continuous_scale=color_scale, featureidkey="properties.sigla",
                            center={"lat": -15, "lon": -47},
                            hover_name='Province_State',
-                           range_color=(0, 50000),
+                           range_color=range_color,
                            scope='south america', template='plotly_dark',
-                           labels={day:f'{options}'}
+                           labels={day:f'{option}'}
                           )
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
     return fig
 
-data = brazilian_confirmed_cases
+# show by default the actual day
+day = get_actual_day()
 
-if options == 'Número de Mortos':
-    data = brazilian_deaths_cases
-elif options == 'Casos Recuperados':
-    data = brazilian_recovered_cases
-elif options == 'Casos ativos':
-    data = brazilian_active_cases
+# filter interest to show in graph
+data_to_show = filter_data_distribution_to_show(option_selected)
 
-day = '05-23-2020'
 # plot data distribution
-figure = make_graph_map(data, options, day)
+figure = make_graph_map(data_to_show, option_selected, day)
+
 st.plotly_chart(figure)
--- src/analyzer.py
+++ src/analyzer.py
@@ -1,5 +1,19 @@
-import plotly.express as px
+from datetime import date, timedelta
+import pandas as pd
 import folium
+import plotly.express as px
+
+def get_brazilian_confirmed_cases():
+    return pd.read_csv('../data/brazilian covid-19 data/brazilian_confirmed_series.csv')
+
+def get_brazilian_death_cases():
+    return pd.read_csv('../data/brazilian covid-19 data/brazilian_deaths_series.csv')
+
+def get_brazilian_recovered_cases():
+    return pd.read_csv('../data/brazilian covid-19 data/brazilian_recovered_series.csv')
+
+def get_brazilian_active_cases():
+    return pd.read_csv('../data/brazilian covid-19 data/brazilian_active_series.csv')
 
 def clean_unknown_rows_covid_19_df(df_region_covid_data):
 
@@ -49,3 +63,35 @@ def make_graph_map(region_geojson, dataFrame):
     fig.write_html('../graphics/brazilian_confirmed_cases.html')
 
     return fig
+
+def get_actual_day():
+    return (date.today() - timedelta(1)).strftime('%m-%d-%Y')
+
+def filter_data_distribution_to_show(options):
+
+    if options == 'NÚMERO DE MORTOS':
+        return get_brazilian_death_cases()
+    elif options == 'CASOS RECUPERADOS':
+        return get_brazilian_recovered_cases()
+    elif options == 'CASOS ATIVOS':
+        return get_brazilian_active_cases()
+    else:
+        return get_brazilian_confirmed_cases()
+
+def filter_range_color(option):
+
+    if option == 'NÚMERO DE MORTOS':
+        return (0, 2500)
+    elif option == 'CASOS RECUPERADOS':
+        return (0, 50)
+    elif option == 'CASOS ATIVOS':
+        return (0, 50000)
+    else:
+        return (0, 40000)
+
+def filter_map_color(option):
+
+    if option == 'CASOS RECUPERADOS':
+        return 'Greens'
+    else:
+        return 'OrRd'
