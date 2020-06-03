4d7e2feaad1162623b2d52b349b06dcf38df9cc2
Ronaldo Junior <ronaldo@Debian>
2020-06-03 20:54:19 -0300

    adding input data and removing slider

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ import json, sys
 sys.path.append('/home/ronaldo/Documentos/Python Projects/Covid-19 Analyzer/src/')
 import streamlit as st
 import plotly.express as px
-from analyzer import get_number_of_collect_days, convert_day_number_to_date
+from analyzer import get_initial_day, date_format, get_last_day_of_collection
 from analyzer import filter_data_distribution_to_show, filter_range_color, filter_map_color
 
 # st.cache tag indicate that func must be stay in cache to speed up process
@@ -13,7 +13,18 @@ def get_brazilian_geo_data():
 
 brazilian_geo_data = get_brazilian_geo_data()
 
-st.title("Analisando o impacto da COVID-19 nos estados brasileiros")
+# Side bar Menu
+st.sidebar.title("Análise")
+
+button_analyze = st.sidebar.radio("Selecione o tipo de análise que deseja realizar", 
+                ('ESTADOS', 'CIDADES'))
+
+st.sidebar.title("Data de Análise")
+
+days_ago = st.sidebar.date_input(f'Selecione a data que deseja analisar', value=get_last_day_of_collection(), min_value=get_initial_day(), max_value=get_last_day_of_collection())
+
+
+st.title(f"Analisando o impacto da COVID-19 no Brasil")
 
 # subtitle
 st.markdown("Este Data App foi construido para analisar os diferentes cenários de COVID-19 no Brasil")
@@ -40,27 +51,17 @@ def make_graph_map(dataFrame, option, day):
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     return fig
 
-
-number_of_days = get_number_of_collect_days() - 1
-
-# show by default the actual day
-days_ago = st.slider(f'LINHA TEMPORAL DE {option_selected}', (number_of_days * -1), -1, format=f'%d Dias')
-
-day = convert_day_number_to_date(days_ago)
+day = date_format(days_ago)
 
 # filter interest to show in graph
 data_to_show = filter_data_distribution_to_show(option_selected)
 
-with st.spinner('Carregando gráfico'):
+with st.spinner('Carregando gráfico...'):
   # plot data distribution
-  figure = make_graph_map(data_to_show, option_selected, day)
-st.success('Concluído!')
+  if button_analyze == 'ESTADOS':
+    figure = make_graph_map(data_to_show, option_selected, day)
+  else:
+    pass
+    # make figure to cities
 
 st.plotly_chart(figure)
-
-
-# Side bar Menu
-st.sidebar.title("Análise")
-
-botao_selected = st.sidebar.radio("Selecione o tipo de análise que deseja realizar", 
-                ('ESTADOS', 'CIDADES'))
