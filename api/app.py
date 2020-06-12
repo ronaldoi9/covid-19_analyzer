6ad3d539cb2809adf67d1a47905197d1ced682b4
Ronaldo Junior <ronaldo@Debian>
2020-06-11 22:52:41 -0300

    adding comparison between cities

--- api/app.py
+++ api/app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import plotly.express as px
 import pydeck as pdk
 import numpy as np
+from os import path
 from analyzer import get_initial_day, date_format, get_last_day_of_collection, get_cities_by_state, get_lat_and_long, get_covid_19_city_data, get_capital_index
 from analyzer import get_dataframe_selected_by_option, filter_range_color, filter_map_color, filter_historic_df_by_cities
 
@@ -55,11 +56,15 @@ st.title(f"Analisando o impacto da COVID-19 no Brasil")
 st.markdown("Este Data App foi construido para analisar os diferentes cenários de COVID-19 no Brasil")
 
 if button_analyze == 'CIDADES':
+
+  city_data = []
+  df_cities = get_brazilian_historic_data_frame()
+
   option_selected = st.selectbox(
   'Selecione a opção que deseja analisar',
   ('CASOS ACUMULADOS', 'CASOS NOVOS', 'ÓBITOS ACUMULADOS', 'ÓBITOS NOVOS'))
 
-  st.subheader('Informe o nome da cidade e o estado que deseja analisar')
+  st.subheader('Informe o nome da cidade que deseja analisar')
 
   cities_by_state = get_cities_by_state(state_choised, brazilian_cities_geo_data)
   capital_index = get_capital_index(state_choised, cities_by_state)
@@ -68,10 +73,45 @@ if button_analyze == 'CIDADES':
     'Selecione ou digite o nome da cidade', 
     cities_by_state, index=capital_index)
 
+  lat_and_lon = get_lat_and_long(city_choised, brazilian_cities_geo_data)
+  covid_19_city_data = get_covid_19_city_data(df_cities, city_choised, option_selected)
+
+  city_data.append({
+      'n_casos': covid_19_city_data,
+      'lat': lat_and_lon['lat'],
+      'lon': lat_and_lon['lon']
+  })
+
+  compare_city = st.radio(
+      "Deseja comparar cidades?",
+      ('Sim', 'Não'), index=1)
+
+  if compare_city == 'Sim':
+
+    index_to_exclude = list(cities_by_state).index(city_choised)
+    cities_by_state = list(cities_by_state)
+
+    del((cities_by_state[index_to_exclude]))
+
+    city_to_compare = st.selectbox(
+      'Selecione ou digite o nome da cidade para comparar', 
+      cities_by_state, index=capital_index+1)
+
+    if st.button('Adicionar cidade no gráfico'):
+      covid_19_city_data = get_covid_19_city_data(df_cities, city_to_compare, option_selected)
+      lat_and_lon = get_lat_and_long(city_to_compare, brazilian_cities_geo_data)
+
+      city_data.append({
+        'n_casos': covid_19_city_data,
+        'lat': lat_and_lon['lat'],
+        'lon': lat_and_lon['lon']
+      })
 else:
   option_selected = st.selectbox(
-  'Selecione a opção que deseja analisar',
-  ('CASOS CONFIRMADOS', 'NÚMERO DE MORTOS', 'CASOS RECUPERADOS', 'CASOS ATIVOS'))
+    'Selecione a opção que deseja analisar',
+    ('CASOS CONFIRMADOS', 'NÚMERO DE MORTOS', 'CASOS RECUPERADOS', 'CASOS ATIVOS'))
+
+  data_to_show = get_dataframe_selected_by_option(option_selected)
 
 st.subheader("COVID-19 no Brasil")
 
@@ -112,42 +152,31 @@ def make_cities_graph(cities_data):
           extruded=True,
           coverage=1))
 
+    if (cities_data[i]['n_casos']) > 60000:
+      zoom = 9
+    elif (cities_data[i]['n_casos']) > 23000:
+      zoom = 10
+    else:
+      zoom = 11
+
   st.pydeck_chart(pdk.Deck(
     map_style='mapbox://styles/mapbox/light-v9',
     initial_view_state=pdk.ViewState(
         latitude=cities_data[len(cities_data) - 1]['lat'],
         longitude=cities_data[len(cities_data) - 1]['lon'], 
-        zoom=11,
+        zoom=zoom,
         pitch=50),
     layers=[layers],
     ))
   
-
 day = date_format(days_ago)
-
-# check if analyze is about cities or states
-if button_analyze == 'CIDADES':
-  df_cities = get_brazilian_historic_data_frame()
-  # filter by option
-else:
-  data_to_show = get_dataframe_selected_by_option(option_selected)
-
 
 with st.spinner('Carregando gráfico...'):
   # plot data distribution
   if button_analyze == 'ESTADOS':
     figure = make_graph_map(data_to_show, option_selected, day)
     st.plotly_chart(figure)
-  else:
-    lat_and_lon = get_lat_and_long(city_choised, brazilian_cities_geo_data)
-    covid_19_city_data = get_covid_19_city_data(df_cities, city_choised, option_selected)
-
-    city_data = [{
-      'n_casos': covid_19_city_data,
-      'lat': lat_and_lon['lat'],
-      'lon': lat_and_lon['lon']
-    }]
-
+  else:    
     st.info(f'{option_selected}: {covid_19_city_data}')
     make_cities_graph(city_data)
 
